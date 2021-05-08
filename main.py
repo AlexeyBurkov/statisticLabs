f48159e6c494aef6fec4e136caed1eb7a16e4300
Burkov Alexey <rubicks_cube@mail.ru>
2021-05-09 01:39:06 +0300

    lab 5 finished

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ from lab4 import do_func_plus_density_research
 from lab5 import do_correlation_research, do_ellipse_research
 
 if __name__ == '__main__':
-    do_ellipse_research()
+    do_correlation_research()
+    # do_ellipse_research()
